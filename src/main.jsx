59e7d8f400825510434817db8649a5aed5069e9b
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-03-11 16:01:05 +0200

    feat: add ci workflow file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router.jsx'
-//asd
+
 createRoot(document.getElementById('root')).render(
     // <StrictMode>
 
