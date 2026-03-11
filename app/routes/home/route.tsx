f58e052fb8917996d021c066fd6b8a02eb90357c
Vitalii Dylovyi <vitalii.dylovyi@IceMobile.com>
2026-03-11 23:04:09 +0200

    re-init project with ts

---
+++ app/routes/home/route.tsx
@@ -0,0 +1,9 @@
+import { Button } from "~/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex min-h-svh flex-col">
+      <div className="mx-5 mt-5 h-[970px] rounded-4xl bg-foreground"></div>
+    </div>
+  )
+}
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,3 +0,0 @@
-export const NotFoundPage = () => {
-    return <h1>NOT FOUND PAGE</h1>
-}
