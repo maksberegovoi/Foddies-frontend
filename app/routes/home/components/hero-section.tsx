0db0771df364cb2c836b4f133dec3dd250b1868b
Dmytro Popovskyi <popovskiy96@gmail.com>
2026-03-15 02:49:51 +0100

    done hero markup

--- app/routes/home/components/hero-section.tsx
+++ app/routes/home/components/hero-section.tsx
@@ -1,39 +1,67 @@
 import { Button } from "~/components/ui/button"
-import { cn } from "~/lib/utils"
 import HeroBig from "~/assets/images/hero_big.png"
 import HeroSmall from "~/assets/images/hero_small.png"
+import { cn } from "~/lib/utils"
 
 export default function HeroSection() {
   return (
     <section
       className={cn(
-        "mx-5 mt-5 flex flex-col items-center rounded-4xl bg-foreground"
+        "mx-2 mt-2 rounded-4xl bg-foreground px-4 pt-48.5 pb-30",
+        "min-[768px]:mx-4 min-[768px]:mt-4 min-[768px]:px-7.25 min-[768px]:pt-54.25",
+        "min-[1440px]:mx-5 min-[1440px]:mt-5 min-[1440px]:pt-38.5 min-[1440px]:pb-27.5"
       )}
     >
-      <div className="flex max-w-4xl flex-col items-center gap-10 pt-[154px]">
-        <h1 className="text-center text-[90px] leading-[90px] font-extrabold text-white uppercase">
+      <div className="mx-auto max-w-81.75 min-[768px]:w-169.5 min-[768px]:max-w-none min-[1440px]:w-218.75">
+        <h1
+          className={cn(
+            "mb-5 justify-start text-center text-[40px] leading-none font-extrabold text-white uppercase",
+            "min-[768px]:mb-10 min-[768px]:text-[70px]",
+            "min-[1440px]:text-[90px]"
+          )}
+        >
           Improve Your Culinary Talents
         </h1>
 
-        <span className="max-w-xl text-center font-light text-white">
+        <p
+          className={cn(
+            "mx-auto mb-5 text-center text-[14px] leading-[1.43] font-medium text-white",
+            "min-[768px]:mb-10 min-[768px]:w-144.25 min-[768px]:text-[16px] min-[768px]:leading-normal"
+          )}
+        >
           Amazing recipes for beginners in the world of cooking, enveloping you
           in the aromas and tastes of various cuisines.
-        </span>
+        </p>
 
         <Button
-          className="px-8 text-base font-medium uppercase"
+          className={cn(
+            "item-center mx-auto mb-12 flex h-11 w-32.75 justify-center uppercase",
+            "min-[768px]:mb-14.5 min-[768px]:h-14 min-[768px]:w-39.75 min-[768px]:text-base"
+          )}
           variant="outlineWhite"
         >
           Add recipe
         </Button>
 
-        <div className="flex gap-9 pt-17 pb-28">
+        <div className="mx-auto flex w-full items-center justify-center gap-5 min-[768px]:gap-8.75">
           <img
             src={HeroSmall}
             alt=""
-            className="mt-32 h-[116px] w-32 rotate-12"
+            className={cn(
+              "mt-16 h-auto w-[24%] max-w-19.25 rotate-12",
+              "min-[375px]:h-17.5 min-[375px]:w-19.25",
+              "min-[768px]:h-29 min-[768px]:w-32 min-[768px]:max-w-none"
+            )}
           />
-          <img src={HeroBig} alt="" className="w-[302px] -rotate-11" />
+          <img
+            src={HeroBig}
+            alt=""
+            className={cn(
+              "h-auto w-[58%] max-w-47.5 -rotate-11",
+              "min-[375px]:h-43 min-[375px]:w-47.5",
+              "min-[768px]:h-68.25 min-[768px]:w-75.5 min-[768px]:max-w-none"
+            )}
+          />
         </div>
       </div>
     </section>
