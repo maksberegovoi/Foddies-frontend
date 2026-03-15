abc2df083d256289da7ab4120ef5ef60ed7d08e1
Vitalii Dylovyi <158603221+vitalii-dylovyi@users.noreply.github.com>
2026-03-15 17:21:26 +0200

    feat: complete homepage ui, fix sprite (#9)
    Co-authored-by: Vitalii Dylovyi <vitalii.dylovyi@IceMobile.com>

---
+++ app/components/FIcon.tsx
@@ -0,0 +1,35 @@
+import IconsSprite from "~/assets/icons/sprite.svg"
+
+type IconNames =
+  | "arrow-to-left"
+  | "arrow-up-right"
+  | "burger"
+  | "camera"
+  | "checmark"
+  | "chevron-down"
+  | "chevron-up"
+  | "close-x"
+  | "eye-off"
+  | "eye"
+  | "facebook"
+  | "heart"
+  | "instagram"
+  | "minus"
+  | "plus"
+  | "quotes"
+  | "trash"
+  | "youtube"
+
+type FIconProps = {
+  iconName: IconNames
+} & React.SVGProps<SVGSVGElement>
+
+const FIcon = ({ iconName, ...props }: FIconProps) => {
+  return (
+    <svg {...props}>
+      <use href={`${IconsSprite}#${iconName}`} name={iconName}></use>
+    </svg>
+  )
+}
+
+export default FIcon
