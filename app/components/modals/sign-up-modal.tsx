4a35f9d0d5bd5069f231011154533cd1a20957f5
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-03-21 03:50:10 +0200

    c

--- app/components/modals/sign-up-modal.tsx
+++ app/components/modals/sign-up-modal.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
-
 import FIcon from "~/components/FIcon"
 import { Button } from "~/components/ui/button"
 import {
